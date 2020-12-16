d3a5c74716c4dcf0e71c48849c069586219f119b
Tony Doan <ynotdoan@gmail.com>
2020-12-15 17:36:17 -0800

    started binary search code

--- binary_search.py
+++ binary_search.py
@@ -1,1 +1,20 @@
 
+class binary_search():
+  def __init__(self, k, value):
+    self.value = value
+    self.k = []
+    
+  def binary_search(self, k, value):
+    left = 0
+    right = len(k) - 1
+    mid = (left + right) / 2
+    
+    while (value != k[mid]):
+      mid = (left + right) / 2
+      
+      if (value < k[mid]):
+        right = mid - 1
+      elif (value > k[mid]):
+        left = mid + 1
+    
+    return -1
