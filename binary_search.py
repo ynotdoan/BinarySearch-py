a5ccc7b27afeb9f9ec921818c69d0acb3366dd16
Tony Doan <ynotdoan@gmail.com>
2020-12-15 18:01:46 -0800

    added main.py and editted binary search

--- binary_search.py
+++ binary_search.py
@@ -1,18 +1,21 @@
 
 class binary_search():
-  def __init__(self, l, r, mid, k, value):
+  def __init__(self, k, value):
     self.l = 0
     self.r = len(k) - 1
-    self.mid = (l + r) / 2
+    self.mid = (self.l + self.r) / 2
+    self.k = k
+    self.value = value
     
   def binary_search(self, l, r, mid, k, value):
     if (value < k[mid]):
-      r = mid - 1
-      return binary_search(l, r, mid, k, value)
+      self.r = mid - 1
+      binary_search(k, value)
     elif (value > k[mid]):
-      l = mid + 1
-      return binary_search(l, r, mid, k, value)
+      self.l = mid + 1
+      binary_search(k, value)
     elif (value == k[mid]):
-      return True
+      print ("test")
+      return 1
     
     return -1
---
+++ main.py
@@ -0,0 +1,5 @@
+
+import binary_search as bs
+
+nums = [2, 1, 12, 41, 51, 122, 91]
+res = bs.binary_search(nums, 1)
