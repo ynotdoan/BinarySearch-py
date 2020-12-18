4bc17cf565933e5c93bcc2dc7af30597faa11a7e
Tony Doan <ynotdoan@gmail.com>
2020-12-18 15:53:04 -0800

    added tests.py but tests wont run

---
+++ tests.py
@@ -0,0 +1,16 @@
+
+import unittest
+import binary_search as b
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 6, 4, 3, 42, 34 ]
+
+class BinarySearchTest(unittest.TestCase):
+  '''
+  Test cases for class BinarySearch.
+  '''
+  def setUp(self):
+    self.search = b.BinarySearch(nums, 42)
+    
+  def test_binary_search(self):
+    self.search.b_search()
+    self.assertEqual(self.search, 1)
