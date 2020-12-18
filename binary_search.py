6c2516a4fd9125d6a8fd8249125d6c62070cbc58
Tony Doan <ynotdoan@gmail.com>
2020-12-18 15:36:37 -0800

    editted BS to work and returns correct answer

--- binary_search.py
+++ binary_search.py
@@ -7,22 +7,27 @@ class BinarySearch():
     self.l = 0
     self.r = len(k) - 1 
     self.mid = (self.l + self.r) // 2
-    self.k = k
+    self.k = sorted(k)
     self.value = value
     
   def b_search(self):
     '''
     Compares value to mid value and sets new points until value is found.
     '''
-    while (self.value != self.k[self.mid]):
+    print (self.k)
+    while (self.l <= self.r):
       self.mid = (self.l + self.r) // 2
+      print (f"{self.k[self.mid]} {self.mid} {self.value}")
       if (self.value < self.k[self.mid]):
         self.r = self.mid - 1
         
       elif (self.value > self.k[self.mid]):
         self.l = self.mid + 1
         
+      elif (self.value == self.k[self.mid]):
+        break
+       
     if (self.value == self.k[self.mid]):
-      print (f"Value {self.value} found at index {self.mid}")
+      return True
     else:
-      print (f"Value {self.value} not found in given list")
+      return False
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from binary_search import BinarySearch as BS
 
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-res = BS(nums, 12)
-res.b_search()
+nums = [1, 11, 3, 4, 5, 6, 7, 8, 9, 2, 4]
+res = BS(nums, 2)
+print (res.b_search())
